cf168d00e03370569d559b1c3cf08cb80b082a4b
Mohamed Alaa <mohamed.alaa.elhefny@gmail.com>
2024-05-15 18:33:04 +0300

    add process.env

--- src/app/Services/Exams/exams.service.ts
+++ src/app/Services/Exams/exams.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'environment';
+import { environment } from 'src/app/Environment/environment';
 import { Observable } from 'rxjs';
 import { IExam } from 'src/app/model/iexam';
 
--- src/app/Services/teacher/teacher.service.ts
+++ src/app/Services/teacher/teacher.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'environment';
+import { environment } from 'src/app/Environment/environment';
 import { Observable, map } from 'rxjs';
 
 @Injectable({
